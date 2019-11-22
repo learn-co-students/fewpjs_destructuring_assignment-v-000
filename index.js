a50adb571ceb176d5400c0481489757853da217d
github username <github email address>
2019-11-21 16:14:18 -0800

    Done.

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-const farmAnimals = 'cow horse sheep pig chicken'
+const farmAnimals = "cow horse sheep pig chicken";
 
-const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
 
 const muppet = {
-  name: 'Kermit',
-  color: 'green',
-  song: 'The Rainbow Connection',
-  job: 'Host of The Muppet Show',
-  partner: 'Miss Piggy'
+  name: "Kermit",
+  color: "green",
+  song: "The Rainbow Connection",
+  job: "Host of The Muppet Show",
+  partner: "Miss Piggy"
 };
 
 const k_muppet = {
-  k_name: 'Kermit',
-  k_color: 'green',
+  k_name: "Kermit",
+  k_color: "green",
   k_album: {
     the_muppet_movie: {
-      song_1: 'Rainbow Connection',
-      song_2: 'Moving Right Along',
-      song_3: 'Never Before, Never Again',
-      song_4: 'I Hope That Something Better Comes Along',
-    },
+      song_1: "Rainbow Connection",
+      song_2: "Moving Right Along",
+      song_3: "Never Before, Never Again",
+      song_4: "I Hope That Something Better Comes Along"
+    }
   },
-  k_job: 'Host of The Muppet Show',
-  k_partner: 'Miss Piggy'
+  k_job: "Host of The Muppet Show",
+  k_partner: "Miss Piggy"
 };
 
 // Strings
@@ -39,11 +39,11 @@ const k_muppet = {
 
 // 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
 
-// 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
+// 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg.
 
 // Objects
 
 // 7. Use destructuring to assign all appropriate variables using the keys as the variable names
 
-
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+const array = [];
